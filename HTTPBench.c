74a1aaf91ad30267d95310bffbbd09167d4b35b6
mjtakenon <cs15503@s.inf.shizuoka.ac.jp>
2017-06-06 15:42:35 +0900

    benchmark modifired

--- HTTPBench.c
+++ HTTPBench.c
@@ -1,98 +1,86 @@
-#include "exp1_http.h"
-
-char g_hostname[256];
-pthread_mutex_t g_mutex;
-int g_error_count;
-
-void exp1_session_error()
-{
-  pthread_mutex_lock(&g_mutex);
-  g_error_count++;
-  pthread_mutex_unlock(&g_mutex);
-}
-
-double exp1_gettimeofday_sec()
-{
-  struct timeval tv;
-  gettimeofday(&tv, NULL);
-  return tv.tv_sec + tv.tv_usec * 1e-6;
-}
-
-void* exp1_eval_thread(void* param)
-{
-  int sock;
-  int* pfileid;
-  int fileid;
-  char command[1024];
-  char buf[2048];
-  int total;
-  int ret;
-
-  pfileid = (int*) param;
-  fileid = *pfileid;
-  free(pfileid);
-
-  sock = exp1_tcp_connect(g_hostname, 15503);
-  if(sock < 0){
-    exp1_session_error();
-    return;
-  }
-
-  sprintf(command, "GET /%03d.jpg HTTP/1.0\r\n\r\n", fileid);
-  ret = send(sock, command, strlen(command), 0);
-  if(ret < 0){
-    exp1_session_error();
-    return;
-  }
-
-  total = 0;
-  ret = recv(sock, buf, 2048, 0);
-  while(ret > 0){
-    total += ret;
-    ret = recv(sock, buf, 2048, 0);
-  }
-}
+#include "exp1.h"
+#include <unistd.h>
 
 int main(int argc, char** argv)
 {
-  int num;
+  int num,ps_num, tstart;
+  int i,j,opt;
+  int errorcount = 0;
+  int aopt = 0;
+  int bopt = 0;
+  double total_average = 0;
+  double tcp_average = 0;
+  double http_average = 0;
   pthread_t *th;
-  int i;
-  double start, end;
-
-  if(argc != 3){
-    printf("usage: %s [ip address] [# of clients]\n", argv[0]);
+  sdata ***data;
+  
+  if(argc != 5){
+    printf("usage: %s [ip address] [# of thread] [# of process] [port]\n", argv[0]);
     exit(-1);
   }
-
   strcpy(g_hostname, argv[1]);
 
   g_error_count = 0;
   pthread_mutex_init(&g_mutex, NULL);
-
-  num = atoi(argv[2]);
+ 
+  num = atoi(argv[2]);  
+  ps_num = atoi(argv[3]);
   th = malloc(sizeof(pthread_t) * num);
-
-  start = exp1_gettimeofday_sec();
-
+  
+  data = (sdata ***)malloc(sizeof(sdata **) * num);
+  for(i = 0;i < num;i++){
+    data[i] = (sdata **)malloc(sizeof(sdata *) * ps_num);
+    for(j = 0; j < ps_num; j++){
+      data[i][j] = (sdata *)malloc(sizeof(sdata));
+    }
+  }
   srand(0);
   for(i = 0; i < num; i++){
-    int fileid = rand() % 100;
-    int *pfileid = malloc(sizeof(int));
-    *pfileid = fileid;
-    pthread_create(&th[i], NULL, exp1_eval_thread, pfileid);
+    for(j = 0; j < ps_num; j++){
+      int fileid = rand() % 100;
+      data[i][j]->pfileid = fileid;
+      data[i][j]->port = atoi(argv[4]);
+      data[i][j]->start = exp1_gettimeofday_sec();
+      data[i][j]->ps_num = ps_num;
+      data[i][j]->is_error = 0;
+    }
+    pthread_create(&th[i], NULL, exp1_eval_thread, data[i]);
   }
 
   for(i = 0; i < num; i++){
     pthread_join(th[i], NULL);
   }
 
-  end = exp1_gettimeofday_sec();
-
-  printf("total time is %10.10f\n", end - start);
-  printf("session error ratio is %1.3f\n",
-         (double)g_error_count / (double) num);
-
+  for(i = 0;i < num;i++){
+    for(j = 0; j < ps_num; j++){
+      if(data[i][j]->is_error == 1)
+      {
+        errorcount++;
+        continue;
+      }
+      tcp_average += (data[i][j]->tcp_end - data[i][j]->start);
+      http_average += (data[i][j]->http_end - data[i][j]->start);
+      total_average += (data[i][j]->end - data[i][j]->start);
+    }
+  }
+  for(i = 0;i < num;i++){
+    for(j = 0; j < ps_num; j++){
+      free(data[i][j]);
+    }
+  }
+  for(i = 0;i < num;i++){
+    free(data[i]);
+  }
+  free(data);
   free(th);
-}
 
+  tcp_average /= (num*ps_num)-errorcount;
+  http_average /= (num*ps_num)-errorcount;
+  total_average /= (num*ps_num)-errorcount;
+  
+  printf("tcp:%10.10f\nhttp:%10.10f\ntotal:%10.10f\n",tcp_average,http_average,total_average);
+
+  printf("session error ratio is %1.3f\n",(double)g_error_count / (double) num);
+
+
+}
